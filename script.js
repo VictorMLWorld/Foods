27977a8c8159763280263fa1009a5f8bad7c2c4f
VictorMLWorld <olubunmivictor0@gmail.com>
2022-03-23 17:43:43 -0300

    Foods Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
+const icons = document.querySelectorAll(".section-1-foods img.foods-display");
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+  const icon = document.querySelector(".section-1-foods .change");
+
+  icon.classList.remove("change");
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 3000);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
